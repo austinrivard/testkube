dd32162e7df7da30160b8ebf36c68f2f23096be6
austin <56987226+austinrivard@users.noreply.github.com>
2023-12-28 18:14:29 -0800

    Some test endpoints

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/austinrivard/testkube
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(w, "This is the root\n")
+	})
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+		reqData, err := io.ReadAll(req.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "Hello %s\n", reqData)
+	})
+
+	fmt.Println("Beginning to listen...")
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+
+	http.ListenAndServe(":8081", nil)
+}
